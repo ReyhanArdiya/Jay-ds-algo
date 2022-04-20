5c5fe13536d36463a2e0331a3bc4dc943679f5a4
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-04-20 09:46:14 +0700

    Create `BinarySearchTree.traverseV2`

--- mine/ch-15/binary-search-tree-js/binary-search-tree.js
+++ mine/ch-15/binary-search-tree-js/binary-search-tree.js
@@ -123,7 +123,6 @@ class BinarySearchTree extends TreeNode {
 			// Pick the left one if we haven't visited it yet AND it's not null/undef
 			!visitedNodes.get(topNode.leftChild) &&
 			topNode.leftChild
-
 		) {
 			nextNode = topNode.leftChild;
 		} else if (
@@ -131,7 +130,6 @@ class BinarySearchTree extends TreeNode {
 			// the rightChild yet AND it's not null/undef
 			!visitedNodes.get(topNode.rightChild) &&
 			topNode.rightChild
-
 		) {
 			nextNode = topNode.rightChild;
 		}
@@ -146,6 +144,119 @@ class BinarySearchTree extends TreeNode {
 		);
 	}
 
+	/**
+	 *
+	 * @param {Function} cb
+	 *
+	 * @param {BinarySearchTree} currentNode
+	 *
+	 * @param {Map<BinarySearchTree, {node: BinarySearchTree, totalChildren: number, visited: number }>} visitedNodes
+	 *
+	 * @param {Stack} unfinishedNodes
+	 */
+	traverseV2(
+		cb = node => console.log(node),
+		// Since the first currentNode is root, then automatically root will be the first previousVisited node
+		currentNode = this.#rootNode,
+		visitedNodes = new Map([]),
+		unfinishedNodes = new Stack()
+	) {
+		// In each recursion, we just have to work with one node (currentNode),
+		// and later delegate the nextNode to the next recursion
+
+		// Check if we have visited currentNode before
+		let currentInVisitedNode = visitedNodes.get(currentNode);
+
+		// If we have, we just increment the number of time we visited it
+		if (currentInVisitedNode) {
+			currentInVisitedNode.visited++;
+		} else {
+			// If not, we store it in our memo
+			visitedNodes.set(currentNode, {
+				node          : currentNode,
+				totalChildren : 0,
+				visited       : 1
+			});
+
+			// And change the currentInVisitedNode to be currentNode after it has been saved
+			currentInVisitedNode = visitedNodes.get(currentNode);
+
+			// We increment its totalChildren
+			if (currentInVisitedNode.node.leftChild) {
+				currentInVisitedNode.totalChildren++;
+			}
+
+			if (currentInVisitedNode.node.rightChild) {
+				currentInVisitedNode.totalChildren++;
+			}
+		}
+
+		// We only pass it to cb if we visited the current node for the first time
+		if (currentInVisitedNode.visited === 1) {
+			cb(currentNode);
+		}
+
+		// We'll get the top node in our stack memo to work on next
+		let topNodeInStack = unfinishedNodes.read;
+
+		// If the current node has any children AND
+		// the current topNodeInStack is not the current
+		// (as to avoid pushing the same node twice to the stack)...
+		if (
+			currentInVisitedNode.totalChildren &&
+			topNodeInStack?.node !== currentNode
+		) {
+			// ...We push it to the stack so that we can track that we should visit
+			// this node again until we have visited all of its children
+			unfinishedNodes.push(currentInVisitedNode);
+
+			// We also change topNodeInStack so that we can work with the latest
+			// top node if it was just pushed previously
+			topNodeInStack = unfinishedNodes.read;
+		}
+
+		// If the number of times we have visited this topNode is more than
+		// the total of its children, it means we have visited all of its children
+		if (topNodeInStack.visited > topNodeInStack.totalChildren) {
+			// Therefore, we can pop it from the stack to indicate that we are finished
+			// with this node since we have visited of its children
+			unfinishedNodes.pop();
+
+			// And we also change topNodeInStack so that we can work with the
+			// latest top node since it was changed by popping something before
+			topNodeInStack = unfinishedNodes.read;
+		}
+
+		// The base case when stack is empty, which means we have visited all
+		// of our tree's nodes, therefore we didn't push anything new again before this line
+		if (!unfinishedNodes.read) {
+			return;
+		}
+
+		// The nextNode we are going to work on could be...
+
+		// ...The topNodeInStack (the reason why we access the node property is because
+		// we store its memory reference there, since the schema in our stack is the node,
+		// the node's totalChildren and the number of times we have visited the node)
+		let nextNode = topNodeInStack.node;
+		if (
+			topNodeInStack.node.leftChild &&
+			!visitedNodes.get(topNodeInStack.node.leftChild)
+		) {
+			// ... the node's leftChild if the topNode has a leftChild and we haven't visited it yet
+			nextNode = topNodeInStack.node.leftChild;
+		} else if (
+			topNodeInStack.node.rightChild &&
+			!visitedNodes.get(topNodeInStack.node.rightChild)
+		) {
+			// ... the node's rightChild if the topNode has a rightChild and we haven't visited it yet
+			nextNode = topNodeInStack.node.rightChild;
+		}
+
+		// Then finally, we recurse with the nextNode to work with
+		return this.traverseV2(cb, nextNode, visitedNodes, unfinishedNodes);
+	}
+
 	get rootNode() {
 		return this.#rootNode;
 	}
@@ -153,20 +264,19 @@ class BinarySearchTree extends TreeNode {
 
 const biSTree = new BinarySearchTree(
 	50, // root
-	// BUG An error happens if the tree is only 50, 25 and 75
 	new BinarySearchTree(
-		25, // left 1
+		25,
 		new BinarySearchTree(
-			10, // left 12
-			new BinarySearchTree(4), // left 22
-			new BinarySearchTree(11) // right 22
+			10,
+			new BinarySearchTree(4),
+			new BinarySearchTree(11)
 		),
 		new BinarySearchTree(
 			33,
-			// BUG why does adding any children here stop the traverseV1 recursion early?
-			new BinarySearchTree(20),
-			new BinarySearchTree(50)
-		) // right 12
+
+			new BinarySearchTree(30),
+			new BinarySearchTree(40)
+		)
 	),
 	new BinarySearchTree(
 		75,
@@ -180,8 +290,49 @@ const biSTree = new BinarySearchTree(
 			new BinarySearchTree(82),
 			new BinarySearchTree(95)
 		)
-	) // right 1
+	)
+); // This works with traverseV2!
+
+const biSTree2 = new BinarySearchTree(
+	50, // root
+	new BinarySearchTree(25),
+	new BinarySearchTree(75)
+); // This works with traverseV2!
+
+// This works with traverseV2! But the it will colog undefined on the left node,
+// but isn't that supposed to happen tho since technically there is a tree there
+// but no value
+const biSTree3 = new BinarySearchTree(
+	50, // root
+	new BinarySearchTree(),
+	new BinarySearchTree(75)
+);
+
+// This works with traverseV2! But the it will colog undefined on the right node,
+// but isn't that supposed to happen tho since technically there is a tree there
+// but no value
+const biSTree4 = new BinarySearchTree(
+	50, // root
+	new BinarySearchTree(25),
+	new BinarySearchTree()
+);
+
+const biSTree5 = new BinarySearchTree(
+	50, // root
+	new BinarySearchTree(25),
+	// new BinarySearchTree()
 );
 
 // Traversing the tree should print out in order => 50, 25, 10, 4, 11, 33, 30, 40, 75, 56, 52, 61, 89, 82, 95
 // biSTree.traverseV1(node => console.log(node.data));
+console.log("=====");
+biSTree.traverseV2(node => console.log(node.data));
+console.log("=====");
+biSTree2.traverseV2(node => console.log(node.data));
+console.log("=====");
+biSTree3.traverseV2(node => console.log(node.data));
+console.log("=====");
+biSTree4.traverseV2(node => console.log(node.data));
+console.log("=====");
+biSTree5.traverseV2(node => console.log(node.data));
+console.log("=====");
